138ac21e560ef393db286fff9d96b2272a4ea7e0
loyher343 <loyhernandez59@gmail.com>
2022-02-22 15:05:43 -0600

    reddit api 200

--- redditData.py
+++ redditData.py
@@ -3,9 +3,10 @@ from secrets import *
 base_url = 'https://www.reddit.com/'
 data = {'grant_type': 'password', 'username': REDDIT_USERNAME, 'password': REDDIT_PASSWORD}
 auth = requests.auth.HTTPBasicAuth(APP_ID, APP_SECRET)
-r = requests.post(base_url + 'api/v1/acces_token',
-	data=data,
-	headers={'user-agent': 'APP_NAME by REDDIT_USERNAME'},
-	auth=auth)
+r = requests.post(base_url + 'api/v1/access_token',
+    data=data,
+    headers={'user-agent': 'APP-NAME by REDDIT-USERNAME'},
+	auth=auth
+)
 d = r.json()
 print(d)
