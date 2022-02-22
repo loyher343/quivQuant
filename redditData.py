86ab49721599e6bf92e988edf6d839c7da185590
loyher343 <loyhernandez59@gmail.com>
2022-02-22 16:47:55 -0600

    get wsb posts

--- redditData.py
+++ redditData.py
@@ -1,5 +1,7 @@
-import requests
+import requests, json
 from secrets import *
+
+#get token
 base_url = 'https://www.reddit.com/'
 data = {'grant_type': 'password', 'username': REDDIT_USERNAME, 'password': REDDIT_PASSWORD}
 auth = requests.auth.HTTPBasicAuth(APP_ID, APP_SECRET)
@@ -9,4 +11,43 @@ r = requests.post(base_url + 'api/v1/access_token',
 	auth=auth
 )
 d = r.json()
-print(d)
+#print(d)
+
+#api access
+token = 'bearer ' + d['access_token']
+api_url = 'https://oauth.reddit.com'
+headers = {'Authorization': token, 'User-Agent': 'APP-NAME by REDDIT-USERNAME'}
+
+# #api test
+# response = requests.get(api_url + '/api/v1/me', headers=headers)
+# if response.status_code == 200:
+#     print(response.json()['name'], response.json()['comment_karma'])
+
+# print(r.status_code)
+
+# payload = {'q': 'puppies', 'limit': 5, 'sort': 'relevance'}
+# response = requests.get(api_url + '/subreddits/search', headers=headers, params=payload)
+# print(response.json())
+
+payload = {'limit': 10}
+response = requests.get(api_url + '/r/wallstreetbets/rising', headers=headers, params=payload)
+parse = response.json()
+#['data']['children'][i]['selftext']
+#print(json.dumps(parse['data']['children'], indent = 4))
+posts = response.json()['data']['children']
+#print(json.dumps(posts, indent = 4))
+
+print('/////////////////////////////////////////////////////////////////////////////////////////               ')
+
+list = []
+for i in range(len(posts)):
+    post = posts[i]['data']
+    parsedPosts = { 
+        'title' : post['title'],
+        'upVotes' : post['ups'],
+        'upRation' : post['upvote_ratio'],
+        'text' : post['selftext']
+    }
+    list.append(parsedPosts)
+
+print(list)
